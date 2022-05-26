10adb3dd6d0ce341357c66041f10ce2527b314b8
Jorge Ferrando <jorfermo@gmail.com>
2022-05-26 18:19:00 +0200

    feat: support animated gifs

--- src/main/java/io/ledgerwise/ipfsresizer/service/StorageService.java
+++ src/main/java/io/ledgerwise/ipfsresizer/service/StorageService.java
@@ -3,11 +3,15 @@ package io.ledgerwise.ipfsresizer.service;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
 
 import javax.imageio.ImageIO;
 
+import io.ledgerwise.ipfsresizer.exception.NotSupportedResourceException;
+import io.ledgerwise.ipfsresizer.model.IPFSResource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
@@ -19,21 +23,35 @@ public class StorageService {
    @Value(value = "${output-dir}")
    private String outputDir;
 
-   public void saveImage(BufferedImage image, String cid) throws IOException {
-      String imagePath = "%s_png".formatted(cid);
-      String path = Paths.get(outputDir, imagePath).toString();
-      File outputFile = new File(path);
-      outputFile.getParentFile().mkdirs();
-      ImageIO.write(image, "png", outputFile);
+   public void saveContent(byte[] content, String cid, String suffix) throws IOException {
+      String contentPath = "%s%s_%s".formatted(outputDir, cid, suffix);
+      Path path = Paths.get(contentPath);
+      Files.write(path, content);
    }
 
    public Optional<BufferedImage> getImage(String path) {
-      String imagePath = "%s_png".formatted(path);
       try {
-         BufferedImage image = ImageIO.read(new File(imagePath));
+         BufferedImage image = ImageIO.read(new File(path));
          return Optional.of(image);
       } catch (IOException e) {
          return Optional.empty();
+      }
+   }
+
+   public void saveResource(IPFSResource resource) throws IOException {
+      switch (resource.getType()) {
+         case IMAGE:
+            saveContent(resource.getContent(), resource.getCid(), "png");
+            break;
+         case GIF:
+            saveContent(resource.getContent(), resource.getCid(), "gif");
+            break;
+         case VIDEO:
+            saveContent(resource.getContent(), resource.getCid(), "mp4");
+            break;
+         default:
+            throw new NotSupportedResourceException("%s resource type not supported".formatted(resource.getType()),
+                  null);
       }
    }
 }
